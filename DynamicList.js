3afb68f7b3bafaef0e68e30d7a9285ac19aa228e
Max W Lobato <ml93117@srcs.loc>
2018-03-23 11:51:10 -0700

    everything

---
+++ DynamicList.js
@@ -0,0 +1,23 @@
+var Add_Button = document.getElementById('add');
+
+function add() {
+    var ul = document.getElementById('list');
+    var added = document.createElement('li');
+    ul.appendChild(added);
+
+    var deletbutton = document.createElement('button');
+    var spann = document.createElement('span');
+    added.appendChild(spann);
+    spann.textContent = document.getElementById('new').value;
+    added.appendChild(deletbutton);
+    deletbutton.textContent = "Delete this.";
+
+
+    deletbutton.addEventListener('click', () => {
+        ul.removeChild(added);
+    });
+
+}
+
+
+Add_Button.addEventListener('click', add);
